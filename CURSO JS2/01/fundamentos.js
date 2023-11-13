2c70d14898147cdc1dd293e391785a21182242cb
Sithluis <luiscarlosaraujodapaixao@gmail.com>
2023-11-13 15:29:05 -0300

    fundamentos javascript aula 1

---
+++ CURSO JS2/01/fundamentos.js
@@ -0,0 +1,65 @@
+//Fundamentos
+
+let minhaVariavel = "olá, javascript"
+
+console.log(minhaVariavel)
+
+let meuNumero = 10;
+
+console.log(meuNumero)
+
+console.log(meuNumero + 5)
+
+console.log("10" + 5)
+
+console.log(typeof meuNumero)
+
+console.log(typeof minhaVariavel)
+
+let booleano = true; // or false
+
+console.log(typeof booleano)
+
+let meuObjeto = {};
+let meuArray = [];
+let meuNull = null;
+let meuUndefined = undefined;
+
+console.log(typeof meuObjeto)
+console.log(typeof meuArray)
+console.log(typeof meuNull)
+console.log(typeof meuUndefined)
+
+// let e const
+// novas formas de declarar variáveis
+let x = 10;
+const y = 5;
+
+console.log(typeof x)
+
+console.log(x, y)
+
+// Operadores aritméticos
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+// Operadores de comparação
+console.log(x == y);
+console.log(x != y);
+
+console.log("5" == 5);
+console.log("5" != 5);
+console.log("5" === 5);
+
+// Operadores lógicos
+// AND &&
+// OR ||
+console.log(10 > 5 && 20 > 5);
+console.log(10 > 5 && 20 < 5);
+console.log(10 < 5 && 20 < 5);
+
+console.log(10 > 5 || 20 > 5);
+console.log(10 > 5 || 20 < 5);
+console.log(10 < 5 || 20 < 5);
