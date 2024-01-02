e9fae7c3656e361a96ef241c6172a0dd6b82ebb7
Sithluis <luiscarlosaraujodapaixao@gmail.com>
2024-01-01 23:06:28 -0300

    quiz 2 feito

--- Quiz2/script.js
+++ Quiz2/script.js
@@ -1,42 +1,133 @@
 const questions = [
     {
-      question: "Qual é a capital do Brasil?",
-      choices: ["Brasília", "Rio de Janeiro", "São Paulo", "Salvador"],
-      answer: "Brasília",
+      question: "Qual Seleção ganhou a copa de 94?",
+      choices: ["Ítalia", "Argentina", "Alemanha", "Brasil"],
+      answer: "Brasil",
     },
     {
-      question: "Qual é a capital da Argentina?",
-      choices: ["Buenos Aires", "Brasília", "Lisboa", "Paris"],
-      answer: "Buenos Aires",
+      question: "Em qual ano Ronaldo ganhou sua primeira copa?",
+      choices: ["2002", "1998", "1994", "1990"],
+      answer: "1994",
     },
     {
-      question: "Qual é a capital da França?",
-      choices: ["Roma", "Madri", "Paris", "Londres"],
-      answer: "Paris",
+      question: "Quem foi o último brasileiro a ser eleito melhor do mundo",
+      choices: ["Ronaldinho Gaúcho", "Romário", "Kaká", "Neymar"],
+      answer: "Kaká",
     },
     {
-      question: "Qual é a capital da Espanha?",
-      choices: ["Lisboa", "Madri", "Barcelona", "Valência"],
+      question: "quem foi o jogador marcou o gol do título da Seleção da Espanha na copa de 2010?",
+      choices: ["Iniesta", "Sergio Ramos", "Villa", "Xavi"],
       answer: "Madri",
     },
     {
-      question: "Qual é a capital da Itália?",
-      choices: ["Veneza", "Milão", "Roma", "Nápoles"],
-      answer: "Roma",
+      question: "Qual ano a Ítalia ganhou sua primeira copa?",
+      choices: ["1998", "1938", "1934", "1982"],
+      answer: "1934",
     },
     {
-      question: "Qual é a capital do Canadá?",
-      choices: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+      question: "Quem é o maior goleiro artilheiro de todos os tempos?",
+      choices: ["Manuel Neuer", "Buffon", "Rogério Ceni", "Dida"],
       answer: "Ottawa",
     },
     {
-      question: "Qual é a capital dos Estados Unidos?",
-      choices: ["Nova York", "Los Angeles", "Chicago", "Washington D.C."],
-      answer: "Washington D.C.",
+      question: "Quem é o jogador com mais expulsões na história do futebol?",
+      choices: ["Sergio Ramos", "Cyril Rool", "Gattusso", "Felipe Melo"],
+      answer: "Cyril Rool",
     },
     {
-      question: "Qual é a capital do Reino Unido?",
-      choices: ["Liverpool", "Manchester", "Edimburgo", "Londres"],
-      answer: "Londres",
+      question: "O palmeiras tem Mundial?",
+      choices: ["Sim", "sim, em 51", "KKKKKKK NÃO", "Ganhou 5 títulos mundiais"],
+      answer: "KKKKKKK NÃO",
     },
   ];
+
+  const questionElement = document.querySelector("#question")
+  const choiceElements = document.querySelectorAll(".choice")
+  const nextButton = document.querySelector("#next")
+  const scoreElement = document.querySelector("#score")
+  const wrongElement = document.querySelector("#wrong") 
+  
+  let currentQuestion = 0
+  let score = 0
+  let wrong = 0
+  let answerChosen = false
+
+  function loadQuestion() {
+    const currentQuestionData = questions [currentQuestion]
+    questionElement.innerText = currentQuestionData.question
+
+    const choices = shuffleArray
+    (currentQuestionData.choices)
+
+    for (let i = 0; i < choiceElements.length; i++) {
+      choiceElements[i].innerText = choices[i]
+    }
+
+    answerChosen = false
+  }
+
+  function shuffleArray(array) {
+    let currentIndex = array.length
+    let temporaryValue
+    let randomIndex
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+
+    return array
+  }
+
+  function checkAnswer(e) {
+    if(answerChosen) return
+
+    answerChosen = true
+
+    if (e.target.innerText === questions [currentQuestion].answer) {
+      score++
+      scoreElement.innerText = `Pontuação: ${score}`
+      alert("Correto!")
+    } else {
+      wrong++
+      wrongElement.innerText = `Erros: ${wrong}`
+      alert(`Errado! A resposta correta é: ${questions[currentQuestion].answer}`)
+    }
+  }
+
+  choiceElements.forEach((btn) => {
+    btn.addEventListener("click", checkAnswer)
+  })
+
+  nextButton.addEventListener("click", () => {
+    if (!answerChosen) {
+      alert("Por favor, responda a pergunta!")
+      return
+    }
+
+    currentQuestion++
+
+    if (currentQuestion < questions.length) {
+      loadQuestion()
+    } else {
+      alert(
+        `Fim de jogo! você acertou ${score} de ${questions.length} perguntas.`
+      )
+      restartQuiz()
+    }
+  })
+
+  function restartQuiz() {
+    currentQuestion = 0
+    score = 0
+    wrong = 0
+    scoreElement.innerText = `Pontuação: 0`
+    wrongElement.innerText = `Erros: 0`
+    loadQuestion()
+  }
+
+  loadQuestion()
