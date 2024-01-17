4a8a6d0d032018674b0e6f348ee79cf75a022ce4
Sithluis <luiscarlosaraujodapaixao@gmail.com>
2024-01-17 10:44:35 -0300

    teste

---
+++ CURSO JS3/13_Else_e_Else_if/JS/script.js
@@ -0,0 +1,23 @@
+let nome = "Luis Carlos";
+
+if(nome == "Matheus") {
+    console.log("O nome dele é Matheus");
+} else if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if (nome == "Luis Carlos") {
+    console.log("O nome dele é Luis Carlos");
+}
+else {
+    console.log("Ele possuir outro nome");
+}
+
+let idade = 19;
+
+if(idade >= 20) {
+    console.log("Ele pode entrar na festa");
+} else if (idade >= 18) {
+    console.log("Ele so pode entar na festa com autorização");
+} else if (idade < 18) {
+    console.log("Ele não pode entrar na festa");
+}
+
