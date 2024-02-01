6e14f1742d572890b1974289b62f05ea37fb4e7a
Sithluis <luiscarlosaraujodapaixao@gmail.com>
2024-02-01 14:54:57 -0300

    pesquisar em andamento

---
+++ Projetos para iniciantes/pesquisar episodio/JS/script.js
@@ -0,0 +1,9 @@
+let nome = document.querySelector("#nomeS").value;
+let res = document.querySelector("#res");
+let img = document.createElement("img");
+img.setAttribute('id', 'foto');
+let link = document.createElement("a");
+
+/* continuar amanha a ideia é fazer o link aparecer junto a uma imagem quando for chamada link que levará
+o usuario a outra página com o conteúdo da imagem;
+*/
